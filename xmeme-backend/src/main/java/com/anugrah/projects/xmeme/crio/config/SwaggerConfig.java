75eedb2f7acc69b8037410b0c239338748743327
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-02-05 23:41:26 +0530

    feat: Hosting Swagger UI and The Application on Different Ports         Swagger on 8080         Application on 8081

--- xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/config/SwaggerConfig.java
+++ xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.anugrah.projects.xmeme.crio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +18,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	@Value("${server.port}")
+	private int serverPort;
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.host("localhost:" + serverPort)
 				.ignoredParameterTypes(Pageable.class)
 				.select()
 				.apis(RequestHandlerSelectors.any())
