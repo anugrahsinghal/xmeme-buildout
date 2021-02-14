6bb9503cd3c392239b8ae24d13a7f1a27411c05a
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-02-14 18:43:37 +0530

    fix

--- xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/XmemeBuildoutCrioApplication.java
+++ xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/XmemeBuildoutCrioApplication.java
@@ -2,17 +2,9 @@ package com.anugrah.projects.xmeme.crio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class XmemeBuildoutCrioApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(XmemeBuildoutCrioApplication.class);
-	}
-
+public class XmemeBuildoutCrioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmemeBuildoutCrioApplication.class, args);
