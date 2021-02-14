50e3e3bb7a219e4e7681e541516b43cff53dcbb6
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-02-14 13:46:40 +0530

    fix: logging

--- xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/controller/MemeControllerExceptionHandler.java
+++ xmeme-backend/src/main/java/com/anugrah/projects/xmeme/crio/controller/MemeControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import com.anugrah.projects.xmeme.crio.exceptions.MemeUpdateException;
 import com.anugrah.projects.xmeme.crio.exceptions.MemeValidationException;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,19 +15,24 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class MemeControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(MemeControllerExceptionHandler.class);
+
 	@ExceptionHandler( {MemeNotFoundException.class})
-	void dataNotFound(HttpServletResponse response) {
+	void dataNotFound(HttpServletResponse response, Exception exception) {
+		log.error(exception);
 		response.setStatus(HttpStatus.NOT_FOUND.value());
 	}
 
-	@ExceptionHandler( {MemeUpdateException.class,MemeValidationException.class, ConstraintViolationException.class, IllegalArgumentException.class})
-	void badRequest(HttpServletResponse response) {
+	@ExceptionHandler( {MemeUpdateException.class, MemeValidationException.class, ConstraintViolationException.class, IllegalArgumentException.class})
+	void badRequest(HttpServletResponse response, Exception exception) {
+		log.error(exception);
 //		response.sendError(HttpStatus.BAD_REQUEST.value());
 		response.setStatus(HttpStatus.BAD_REQUEST.value());
 	}
 
 	@ExceptionHandler( {DuplicateMemeException.class})
-	void conflict(HttpServletResponse response) {
+	void conflict(HttpServletResponse response, Exception exception) {
+		log.error(exception);
 //		response.sendError(HttpStatus.BAD_REQUEST.value());
 		response.setStatus(HttpStatus.CONFLICT.value());
 	}
