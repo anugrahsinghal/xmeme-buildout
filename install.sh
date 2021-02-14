b149aab51ef835754d3283c670e1818ed7593f02
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-02-14 14:36:36 +0530

    fix: removed explicit index chore: improve install.sh script chore: improve server-run.sh script

--- install.sh
+++ install.sh
@@ -1,18 +1,39 @@
-# sudo apt-get update
-
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y maven
-
+sudo apt update
+sudo apt upgrade -y
+echo "intsall java"
+sudo apt install openjdk-8-jdk-headless -y
+echo "install java done"
+echo "intsall maven"
+sudo apt install maven -y
+echo "intsall maven done"
+# docker
+echo "intsall docker"
+sudo apt install docker.io -y
+echo "intsall docker done"
+# postgres
+echo "intsall postgres"
 sudo apt-get install -y postgresql postgresql-contrib
+echo "intsall postgres done"
 
-echo "Starting postgres"
-pg_ctl start -l logfile
+echo "start docker"
+sudo systemctl start docker
+echo "docker started"
+
+echo "start postgres"
+sudo systemctl start postgresql
+echo "start postgres done"
+# make changes to postgres
+echo "make postgres changes"
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
 sudo -u postgres psql -c "CREATE DATABASE postgres;"
 sudo -u postgres psql -c "DROP TABLE meme_table;"
-echo "posgres done"
+echo "posgres changes done"
 
-
+echo "build backend"
 cd xmeme-backend
 mvn clean install -Dmaven.test.skip=true
+echo "build backend done"
+
+echo "going back to root repo dir"
 cd -
+echo "going back to root repo dir done"
--- server_run.sh
+++ server_run.sh
@@ -1,3 +1,2 @@
 
-
-java -jar xmeme-backend/target/xmeme-backend.jar >xmeme-backend-info.log 2>xmeme-backend-error.log
+java -jar xmeme-backend/target/xmeme-backend.jar
